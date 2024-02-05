9c348e4c2704ee32254f0b0ee89fdc135f7837c0
uo289792 <uo289792@uniovi.es>
2024-02-05 11:48:19 +0100

    SDI-2324-713-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2324713spring/Sdi2324713SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324713spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324713SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2324713SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2324713spring/Sdi2324713SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324713spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2324713SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
