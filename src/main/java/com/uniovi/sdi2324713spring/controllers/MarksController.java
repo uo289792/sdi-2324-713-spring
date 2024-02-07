22ff0da582d065539dc4049cdcaf742bb32f73f0
uo289792 <uo289792@uniovi.es>
2024-02-07 11:36:35 +0100

    SDI-2324-713-2.3-Beans, servicios e inyección de dependencias.

--- src/main/java/com/uniovi/sdi2324713spring/controllers/MarksController.java
+++ src/main/java/com/uniovi/sdi2324713spring/controllers/MarksController.java
@@ -1,27 +1,37 @@
 package com.uniovi.sdi2324713spring.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.uniovi.sdi2324713spring.entities.Mark;
+import com.uniovi.sdi2324713spring.services.MarksService;
 
 
 @RestController
 public class MarksController {
 
+    @Autowired //Inyectar el servicio
+    private MarksService marksService;
+
     @RequestMapping("/mark/list")
     public String getList() {
-        return "Getting List";
+        return marksService.getMarks().toString();
     }
 
     @RequestMapping(value = "/mark/add", method = RequestMethod.POST)
     public String setMark(@ModelAttribute Mark mark) {
-        return "added: " + mark.getDescription()
-                + " with score : " + mark.getScore()
-                + " id: " + mark.getId();
+        marksService.addMark(mark);
+        return "OK";
     }
 
     @RequestMapping("/mark/details/{id}")
     public String getDetail(@PathVariable Long id) {
-        return "Getting Details =>" + id;
+        return marksService.getMark(id).toString();
+    }
+
+    @RequestMapping("/mark/delete/{id}")
+    public String deleteMark(@PathVariable Long id) {
+        marksService.deleteMark(id);
+        return "OK";
     }
 }
 
