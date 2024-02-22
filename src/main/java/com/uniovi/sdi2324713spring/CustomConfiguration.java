7d849e4c5a3a7ea82f918a39aa47cc30a6befb6f
uo289792 <uo289792@uniovi.es>
2024-02-22 10:37:00 +0100

    SDI-2324-713-sesión5-ejercicio-complementario3

--- src/main/java/com/uniovi/sdi2324713spring/CustomConfiguration.java
+++ src/main/java/com/uniovi/sdi2324713spring/CustomConfiguration.java
@@ -2,6 +2,8 @@ package com.uniovi.sdi2324713spring;
 
 import java.util.List;
 import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -15,6 +17,13 @@ import org.springframework.data.domain.PageRequest;
 
 @Configuration
 public class CustomConfiguration implements WebMvcConfigurer {
+
+    @Value("${spring.data.web.pageable.page-parameter}")
+    private int page;
+
+    @Value("${spring.data.web.pageable.default-page-size}")
+    private int size;
+
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
@@ -34,8 +43,6 @@ public class CustomConfiguration implements WebMvcConfigurer {
     }
 
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        int page = 0;
-        int size = 2;
         PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
         resolver.setFallbackPageable(PageRequest.of(page, size));
         argumentResolvers.add(resolver);
