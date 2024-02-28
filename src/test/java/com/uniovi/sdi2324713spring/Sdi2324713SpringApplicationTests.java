b4539792a25d911d22a5a43448726bf870131772
David <uo289792@uniovi.es>
2024-02-28 11:54:51 +0100

    SDI-2324-713-5.1-Clase principal.

--- src/test/java/com/uniovi/sdi2324713spring/Sdi2324713SpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi2324713spring/Sdi2324713SpringApplicationTests.java
@@ -1,13 +1,58 @@
 package com.uniovi.sdi2324713spring;
 
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class Sdi2324713SpringApplicationTests {
 
-    @Test
-    void contextLoads() {
+    static String PathFirefox = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+    static String Geckodriver = "C:\\Users\\david\\Desktop\\Universidad\\geckodriver-v0.30.0-win64.exe";
+
+    //Común a Windows y a MACOSX
+    static WebDriver driver = getDriver(PathFirefox, Geckodriver);
+    static String URL = "http://localhost:8090";
+    public static WebDriver getDriver(String PathFirefox, String Geckodriver) {
+        System.setProperty("webdriver.firefox.bin", PathFirefox);
+        System.setProperty("webdriver.gecko.driver", Geckodriver);
+        driver = new FirefoxDriver();
+        return driver;
     }
+
+    @BeforeEach
+    public void setUp(){
+        driver.navigate().to(URL);
+    }
+    //Después de cada prueba se borran las cookies del navegador
+    @AfterEach
+    public void tearDown(){
+        driver.manage().deleteAllCookies();
+    }
+    //Antes de la primera prueba
+    @BeforeAll
+    static public void begin() {}
+    //Al finalizar la última prueba
+    @AfterAll
+    static public void end() {
+        //Cerramos el navegador al finalizar las pruebas
+        driver.quit();
+    }
+
+    @Test
+    @Order(1)
+    void p1() {}
+
+    @Test
+    @Order(2)
+    void p2() {}
+
+    @Test
+    @Order(3)
+    void p3() {}
+
 
 }
