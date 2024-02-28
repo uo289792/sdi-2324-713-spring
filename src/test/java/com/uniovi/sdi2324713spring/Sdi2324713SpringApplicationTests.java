653678e9dc8800e77982d0382766a3efe54ceaab
David <uo289792@uniovi.es>
2024-02-28 12:42:52 +0100

    SDI-2324-713-5.3-PO NavView.

--- src/test/java/com/uniovi/sdi2324713spring/Sdi2324713SpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi2324713spring/Sdi2324713SpringApplicationTests.java
@@ -1,10 +1,15 @@
 package com.uniovi.sdi2324713spring;
 
+import com.uniovi.sdi2324713spring.pageobjects.PO_HomeView;
+import com.uniovi.sdi2324713spring.pageobjects.PO_Properties;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -44,15 +49,38 @@ class Sdi2324713SpringApplicationTests {
 
     @Test
     @Order(1)
-    void p1() {}
-
+    void PR01A() {
+        PO_HomeView.checkWelcomeToPage(driver, PO_Properties.getSPANISH());
+    }
     @Test
     @Order(2)
-    void p2() {}
+    void PR01B() {
+        List<WebElement> welcomeMessageElement = PO_HomeView.getWelcomeMessageText(driver,
+                PO_Properties.getSPANISH());
+        Assertions.assertEquals(welcomeMessageElement.get(0).getText(),
+                PO_HomeView.getP().getString("welcome.message", PO_Properties.getSPANISH()));
+    }
 
     @Test
     @Order(3)
-    void p3() {}
+    public void PR02() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+    }
+    //PR03. Opción de navegación. Pinchar en el enlace Identifícate en la página home
+    @Test
+    @Order(4)
+    public void PR03() {
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+    }
+
+    //PR04. Opción de navegación. Cambio de idioma de Español a Inglés y vuelta a Español
+    @Test
+    @Order(5)
+    public void PR04() {
+        PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish",
+                PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+    }
+
 
 
 }
--- src/test/java/com/uniovi/sdi2324713spring/pageobjects/PO_View.java
+++ src/test/java/com/uniovi/sdi2324713spring/pageobjects/PO_View.java
@@ -2,7 +2,7 @@ package com.uniovi.sdi2324713spring.pageobjects;
 
 import java.util.List;
 
-import com.uniovi.notaneitor.util.SeleniumUtils;
+import com.uniovi.sdi2324713spring.util.SeleniumUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
